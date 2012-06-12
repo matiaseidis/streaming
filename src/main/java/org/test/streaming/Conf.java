57ae1799ed1ac29a5012949e0ffd72cedabca4d4
matiaseidis@gmail.com <matiaseidis@gmail.com>
2012-06-11 21:11:45 -0300

    subtitle from servle

--- src/main/java/org/test/streaming/Conf.java
+++ src/main/java/org/test/streaming/Conf.java
@@ -3,13 +3,13 @@ package org.test.streaming;
 public class Conf {
 
 	
-//	public static final String VIDEO = "Luther.S02E01.720p.HDTV.x264-3.mp4";
-//	public static final String VIDEO_DIR = "/home/matias/cachos/files/";
-//	public static final int VIDEO_SIZE = 421732944;
+	public static final String VIDEO = "Luther.S02E01.720p.HDTV.x264-3.mp4";
+	public static final String VIDEO_DIR = "/home/matias/cachos/files/";
+	public static final int VIDEO_SIZE = 421732944;
 	
-	public static final String VIDEO = "Game.of.Thrones.S02E01.HDTV.RM-ASAP.mp4";
-	public static final String VIDEO_DIR = "/home/meidis/Videos/";
-	public static final int VIDEO_SIZE = 393031408;
+//	public static final String VIDEO = "Game.of.Thrones.S02E01.HDTV.RM-ASAP.mp4";
+//	public static final String VIDEO_DIR = "/home/meidis/Videos/";
+//	public static final int VIDEO_SIZE = 393031408;
 	
 	public static final String DIMON_HOST = "localhost";
 	public static final int DIMON_PORT = 10002;
@@ -22,6 +22,7 @@ public class Conf {
 
 	public static final String PREVALENCE_DIR = "streaming-demo/prevalence";
 //	public static final String PREVALENCE_DIR = "/home/matias/servle.prevalence";
+	public static final String SUBS = "Luther-02x01-SP.srt";
 	
 	
 
