77e7c26858887de5975f5fa50f56dfbb42a2783a
Lucas Bernardi <lbernardi@despegar.com>
2012-06-13 15:10:36 -0300

    added null index

--- src/main/java/org/test/streaming/CachoStreamer.java
+++ src/main/java/org/test/streaming/CachoStreamer.java
@@ -4,7 +4,16 @@ import java.io.OutputStream;
 
 public abstract class CachoStreamer extends OutputStream {
 
+	private Index index = new NullIndex();
+
 	public abstract void stream();
 
+	public Index getIndex() {
+		return index;
+	}
+
+	public void setIndex(Index index) {
+		this.index = index;
+	}
 
 }
