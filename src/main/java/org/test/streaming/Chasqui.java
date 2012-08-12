243984d0541c7513d7fd0ad2b4bea86306f8fbfb
Matias Eidis <matiaseidis@gmail.com>
2012-08-12 10:23:52 -0300

    fixed chasqui design

--- src/main/java/org/test/streaming/Chasqui.java
+++ src/main/java/org/test/streaming/Chasqui.java
@@ -1,5 +1,7 @@
 package org.test.streaming;
 
+import java.util.Map;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.test.streaming.monitor.CachoRegistration;
@@ -9,10 +11,13 @@ public class Chasqui implements Index {
 	protected static final Log log = LogFactory.getLog(Chasqui.class);
 	private int indexableSize;
 	private Conf conf;
+	private MovieCachoHasher movieCachoHasher;
+
 	
 	public Chasqui(Conf conf){
 		this.conf = conf;
 		indexableSize = conf.getIndexableSize();
+		this.movieCachoHasher= new MovieCachoHasher(); 
 	}
 	
 	@Override
@@ -23,6 +28,7 @@ public class Chasqui implements Index {
 			return;
 		}
 		
-		new CachoRegistration(movieCachoFile, conf).register();
+		Map<Integer, String> chunksToRegiter = movieCachoHasher.hashMovieCachoFile(movieCachoFile, indexableSize);
+		new CachoRegistration(conf, movieCachoFile, chunksToRegiter).register();
 	}
 }
--- src/main/java/org/test/streaming/monitor/CachoRegistration.java
+++ src/main/java/org/test/streaming/monitor/CachoRegistration.java
@@ -1,16 +1,12 @@
 package org.test.streaming.monitor;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.test.streaming.Conf;
-import org.test.streaming.ChunkHasher;
 import org.test.streaming.MovieCachoFile;
-import org.test.streaming.MovieCachoHasher;
 
 import com.google.gson.Gson;
 
@@ -18,15 +14,15 @@ public class CachoRegistration implements Registration{
 	
 	protected static final Log log = LogFactory.getLog(CachoRegistration.class);
 	private final MovieCachoFile movieCachoFile;
-	private MovieCachoHasher movieCachoHasher;
-	int indexableSize;
+	private int indexableSize;
 	private final Notifier notifier;
 	private Conf conf;
 	private String videoId;
 	private String userId;
 	private ChunkEncoder chunkEncoder;
+	private Map<Integer, String> chunksToRegiter;
 	
-	public CachoRegistration(MovieCachoFile cacho, Conf conf){
+	public CachoRegistration(Conf conf, MovieCachoFile cacho, Map<Integer, String> chunksToRegiter){
 		this.movieCachoFile = cacho;
 		this.conf = conf;
 		
@@ -38,34 +34,22 @@ public class CachoRegistration implements Registration{
 		this.videoId =  conf.get("test.video.file.id"); 
 		this.userId =  conf.get("test.user.id");
 		this.chunkEncoder = new ChunkEncoder();
-		this.movieCachoHasher= new MovieCachoHasher(); 
+		this.chunksToRegiter = chunksToRegiter;
 	} 
 	
 	@Override
 	public RegistrationResponse register() {
 		
-		int cachoFirstByteIndex = movieCachoFile.getCacho().getFirstByteIndex();
-		int cachoLenght = movieCachoFile.getCacho().getLength();
-		
-		int resto = cachoFirstByteIndex % indexableSize;
-		int nextChunkFirstByteIndex = resto == 0 
-						? cachoFirstByteIndex 
-						: cachoFirstByteIndex + resto;
-		
-		int totalChunks = (cachoLenght - (nextChunkFirstByteIndex - cachoFirstByteIndex)) / indexableSize;
-		
-		
 		String fileName = movieCachoFile.getMovieFile().getName();
-		Map<Integer, String> chunksToRegiter = movieCachoHasher.hashMovieCachoFile(movieCachoFile, indexableSize);
 		String chunks = chunkEncoder.encodeChunks(chunksToRegiter);
 		
 		String registrationResponse  = notifier.registerChunks(fileName, this.getUserId(), chunks);
-		log.info("Sended by Chasqui to remote repo <fileName: "+fileName+" - chunks: "+chunks+"]> - "+totalChunks+" fragmentos de "+indexableSize+" reportados");
+		log.info("Sended by Chasqui to remote repo <fileName: "+fileName+" - chunks: "+chunks+"]> - "+chunksToRegiter.size()+" fragmentos de "+indexableSize+" reportados");
 		
 		LinkedHashMap json = new Gson().fromJson(registrationResponse, LinkedHashMap.class);
 		String code = json == null ? "CONNECTION_ERROR" : (String)json.get("code");
 		
-		return new RegistrationResponse(videoId, movieCachoFile.getMovieFile().getName(), code, json.get("body").toString(), 0, movieCachoFile.getCacho().getLength(), totalChunks);
+		return new RegistrationResponse(videoId, movieCachoFile.getMovieFile().getName(), code, json.get("body").toString(), 0, movieCachoFile.getCacho().getLength(), chunksToRegiter.size());
 	}
 
 	public String getUserId() {
